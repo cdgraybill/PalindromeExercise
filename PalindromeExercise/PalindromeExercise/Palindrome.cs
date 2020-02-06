dfacdd0e228cd52341bd0193d4e8872432787732
Cameron Graybill <cdgraybill@gmail.com>
2020-02-06 11:15:58 -0600

    completed exercise

---
+++ PalindromeExercise/PalindromeExercise/Palindrome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class Palindrome
+    {
+        public static bool IsAPalindrome(string word)
+        {
+            string palindrome = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                palindrome += word.ToLower()[i];
+            }
+
+            if (palindrome == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,23 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Racecar", true)]
+        [InlineData("rAcEcAr", true)]
+        [InlineData("Hello World", false)]
+        [InlineData("Tacocat", true)]
+        [InlineData("t a c o c a t", true)]
+        public void TestingForAPalindrome(string word, bool expected)
         {
+            //Arrange
 
+
+            //Act
+            var actual = Palindrome.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
